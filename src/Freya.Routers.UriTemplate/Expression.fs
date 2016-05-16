a5784600c9efcc80f5ce01cf5b5140a49efbe4bb
kolektiv <andrew@xyncro.com>
2016-05-16 10:28:46 +0100

    neater factoring of the extensions and expression to files

--- src/Freya.Routers.UriTemplate/Expression.fs
+++ src/Freya.Routers.UriTemplate/Expression.fs
@@ -2,90 +2,30 @@
 
 open Aether
 open Freya.Core
-open Freya.Core.Operators
 
 (* Expression
 
    A minimal computation expression for defining routers given ordered routes,
    defining a method and path specification, and a pipeline function to be
-   returned when the route is the highest matched route under evaluation. *)
-
-(* Types
-
-   The simple UriTemplateRouter type, defined as a union rather than a simple
-   type alias as the type will have members defined to allow it to be used in
-   static inference. *)
-
-type UriTemplateRouter =
-    | UriTemplateRouter of (UriTemplateRoutes -> unit * UriTemplateRoutes)
-
-(* UriTemplateRouter
-
-   Functions defined against UriTemplateRouter, chiefly the simple monadic init
-   and bind functions to enable the creation of a computation expression
-   builder, plus a function to convert a router to a Pipeline, which will be
-   used when defining the appropriate static members to allow the type to take
-   part in static inference to either Freya<_> or Pipeline functions. *)
-
-[<RequireQualifiedAccess>]
-[<CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
-module UriTemplateRouter =
-
-    (* Common *)
-
-    let init _ : UriTemplateRouter =
-        UriTemplateRouter (fun c ->
-            (), c)
-
-    let bind (m: UriTemplateRouter, f: unit -> UriTemplateRouter) : UriTemplateRouter =
-        UriTemplateRouter (fun c ->
-            let (UriTemplateRouter m) = m
-            let (UriTemplateRouter f) = f ()
-
-            (), snd (f (snd (m c))))
-
-    (* Custom *)
-
-    let map (m: UriTemplateRouter, f: UriTemplateRoutes -> UriTemplateRoutes) : UriTemplateRouter =
-        UriTemplateRouter (fun c ->
-            let (UriTemplateRouter m) = m
-
-            (), f (snd (m c)))
-
-    (* Inference *)
-
-    let pipeline (UriTemplateRouter router) : Pipeline =
-        let (_, UriTemplateRoutes routes) = router (UriTemplateRoutes [])
-        let compilation = compile routes
-
-        evaluate compilation >>= (
-            function | Some (data, pipeline) -> (Route.data_ .= data) *> pipeline
-                     | _ -> Pipeline.next)
+   returned when the route is the highest matched route under evaluation.
+   
+   The UriTemplateRouter type here would ordinarily be defined as part of the
+   general set of types core to the system, but in this case must be defined
+   late to be able to be augmented here due to the F# restriction on type
+   extensions being present in the same file as the type declaration. *)
 
 (* Types
 
    A simple computation expression builder type defined using the simple
-   ConfigurationBuilder inheritance approach provided by Freya.Core. *)
+   ConfigurationBuilder inheritance approach provided by Freya.Core.
+
+   Includes extensions to UriRouterTemplateBuilder to define the custom
+   route operation for declaring routes. *)
 
 type UriTemplateRouterBuilder () =
     inherit ConfigurationBuilder<UriTemplateRouter>
         { Init = UriTemplateRouter.init
           Bind = UriTemplateRouter.bind }
-
-(* Type Extensions
-
-   Type extensions to UriTemplateRouter to define the member functions required
-   for static type inference of Freya<_> and Pipeline types as defined by
-   Freya.Core, and extensions to UriRouterTemplateBuilder to define the custom
-   route operation for declaring routes. *)
-
-type UriTemplateRouter with
-
-    static member Freya (x: UriTemplateRouter) : Freya<_> =
-        UriTemplateRouter.pipeline x
-
-    static member Pipeline (x: UriTemplateRouter) : Pipeline =
-        UriTemplateRouter.pipeline x
 
 type UriTemplateRouterBuilder with
 
---
+++ src/Freya.Routers.UriTemplate/Extension.fs
@@ -0,0 +1,40 @@
+﻿namespace Freya.Routers.UriTemplate
+
+open Freya.Core
+open Freya.Core.Operators
+
+(* Extension
+
+   Type extensions for the UriTemplateRouter to implement common static
+   inference (typeclass-like) functionality defined as part of Freya.Core. *)
+
+[<AutoOpen>]
+module Extension =
+
+    (* Functions
+
+       Reification of a UriTemplateRouter instance to a pipeline, compiling
+       a generated set of routes and returning a function to evaluate the
+       result appropriately. *)
+
+    let private extend (UriTemplateRouter router) : Pipeline =
+        let (_, UriTemplateRoutes routes) = router (UriTemplateRoutes [])
+        let compilation = compile routes
+
+        evaluate compilation >>= (
+            function | Some (data, pipeline) -> (Route.data_ .= data) *> pipeline
+                     | _ -> Pipeline.next)
+
+    (* Extensions
+
+       Implementations of the Freya and Pipeline typeclass-like static
+       inference members defined in Freya.Core. *)
+
+    type UriTemplateRouter with
+
+        static member Freya router : Freya<_> =
+            extend router
+
+        static member Pipeline router : Pipeline =
+            extend router
+
