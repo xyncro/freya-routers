3d78e8dc2587dad8f464af5903fddcd5b1d80068
kolektiv <andrew@xyncro.com>
2016-05-21 17:21:28 +0100

    better factored tests, using Freya.Testing rather than custom harness

---
+++ tests/Freya.Routers.UriTemplate.Tests/Matching.fs
@@ -0,0 +1,92 @@
+﻿[<AutoOpen>]
+module Freya.Routers.UriTemplate.Tests.Matching
+
+open Arachne.Http
+open Freya.Core
+open Freya.Core.Operators
+open Freya.Optics.Http
+open Freya.Routers.UriTemplate
+open Freya.Testing
+open Freya.Testing.Operators
+open Xunit
+
+(* Matching
+
+   Tests covering basic matching of routes to paths and methods. Capture of
+   matched variables is tested explicitly elsewhere. *)
+
+[<Fact>]
+let ``Router With Root Route Executes Correctly`` () =
+
+    let router =
+        freyaRouter {
+            route All "/" (defaultValue .= Some "test") }
+
+    verify defaultSetup router [
+        defaultValue => Some "test" ]
+
+[<Fact>]
+let ``Router With Multiple Routes Executes Correctly`` () =
+
+    let router =
+        freyaRouter {
+            route All "/" (defaultValue .= Some "one")
+            route All "/some/path" (defaultValue .= Some "two")
+            route All "/other/path" (defaultValue .= Some "three") }
+
+    verify (Request.path_ .= "/") router [
+        defaultValue => Some "one" ]
+
+    verify (Request.path_ .= "/some/path") router [
+        defaultValue => Some "two" ]
+
+    verify (Request.path_ .= "/other/path") router [
+        defaultValue => Some "three" ]
+
+[<Fact>]
+let ``Router With Method Specific Routes Executes Correctly`` () =
+
+    let router =
+        freyaRouter {
+            route GET "/" (defaultValue .= Some "one")
+            route GET "/some/path" (defaultValue .= Some "two")
+            route POST "/some/path" (defaultValue .= Some "three") }
+
+    verify (Request.method_ .= GET) router [
+        defaultValue => Some "one" ]
+
+    verify (Request.method_ .= POST) router [
+        defaultValue => None ]
+
+    verify ((Request.method_ .= GET) *> (Request.path_ .= "/some/path")) router [
+        defaultValue => Some "two" ]
+
+    verify ((Request.method_ .= POST) *> (Request.path_ .= "/some/path")) router [
+        defaultValue => Some "three" ]
+
+[<Fact>]
+let ``Router Executes Pipeline Registered First`` () =
+
+    let router =
+        freyaRouter {
+            route GET "/" (defaultValue .= Some "one")
+            route All "/" (defaultValue .= Some "two") }
+
+    verify defaultSetup router [
+        defaultValue => Some "one" ]
+
+[<Fact>]
+let ``Router Executes First Full Match`` () =
+
+    let router =
+        freyaRouter {
+            route All "/{one}/a" (defaultValue .= Some "one")
+            route All "/{two}/b" (defaultValue .= Some "two")
+            route All "/{one}/b" (defaultValue .= Some "three") }
+
+    // TODO: Move the tests for route capture to a distinct file and set of tests!
+
+    verify (Request.path_ .= "/some/b") router [
+        Route.atom_ "one" => None
+        Route.atom_ "two" => Some "some"
+        defaultValue => Some "two" ]
--- tests/Freya.Routers.UriTemplate.Tests/Prelude.fs
+++ tests/Freya.Routers.UriTemplate.Tests/Prelude.fs
@@ -1,55 +1,16 @@
 ﻿[<AutoOpen>]
 module Freya.Routers.UriTemplate.Tests.Prelude
 
-open System.Collections.Generic
 open Aether
 open Freya.Core
 open Freya.Core.Operators
-open Freya.Optics.Http
-open Freya.Routers.UriTemplate
 
-(* Base Data
+(* Optics *)
 
-   Default data instances to initialize tests, commonly empty states for
-   verifying expected application of functions to state. *)
+let defaultValue : Lens<State,string option> =
+    State.value_ "default"
 
-let private environment () =
-    Dictionary<string, obj> () :> IDictionary<string, obj>
+(* Fixtures *)
 
-let private meta () =
-    { Memos = Map.empty }
-
-let private state () =
-    { Environment = environment ()
-      Meta = meta () }
-
-(* Access
-
-   Functions for getting and setting a known value against the state for the
-   purpose of verification. *)
-
-let private lens : Lens<State,int option> =
-    State.value_ "test"
-
-let get =
-    Optic.get lens
-
-let set i =
-        Freya.Optic.set lens i
-     *> Pipeline.next
-
-(* Utilities
-
-   Runner and verification functions. *)
-
-let run meth path query m =
-    Async.RunSynchronously ((   Freya.Optic.set Request.method_ meth
-                             *> Freya.Optic.set Request.path_ path
-                             *> Freya.Optic.set Request.query_ query
-                             *> UriTemplateRouter.Pipeline m) (state ()))
-
-let result meth path query m =
-    fst (run meth path query m)
-
-let value meth path query m =
-    get (snd (run meth path query m))
+let defaultSetup =
+    Freya.empty
