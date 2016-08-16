3f276d3109eb8fbf6de51cc3e50cedca65d91d3a
kolektiv <andrew@xyncro.com>
2016-08-16 18:33:41 +0100

    very basic benchmark

--- benchmarks/Freya.Routers.Uri.Template.Benchmarks/Program.fs
+++ benchmarks/Freya.Routers.Uri.Template.Benchmarks/Program.fs
@@ -30,7 +30,8 @@ let path p =
 let paths =
     List.map path [
         "/"
-        "/one" ]
+        "/one"
+        "/one/two" ]
 
 type Benchmarks () =
 
@@ -41,75 +42,72 @@ type Benchmarks () =
 // Configuration
 
 open BenchmarkDotNet.Configs
-//open BenchmarkDotNet.Diagnostics.Windows
-//open BenchmarkDotNet.Exporters
-//open BenchmarkDotNet.Jobs
-
-(* Instances *)
+open BenchmarkDotNet.Diagnostics.Windows
+open BenchmarkDotNet.Exporters
+open BenchmarkDotNet.Jobs
 
 let configuration =
 
     ManualConfig
         .Create(DefaultConfig.Instance)
 
-//        // Jobs
-//
-//        // Legacy / Any
-//
-//        .With(Job.Default
-//            .With(GarbageCollection (Server = true))
-//            .With(Jit.LegacyJit)
-//            .With(Platform.AnyCpu))
-//
-//        // Ryu / Any
-//
-//        .With(Job.Default
-//            .With(GarbageCollection (Server = true))
-//            .With(Jit.RyuJit)
-//            .With(Platform.AnyCpu))
-//
-//        // Legacy / x86
-//
-//        .With(Job.Default
-//            .With(GarbageCollection (Server = true))
-//            .With(Jit.LegacyJit)
-//            .With(Platform.X86))
-//
-//        // Ryu / x86
-//
-//        .With(Job.Default
-//            .With(GarbageCollection (Server = true))
-//            .With(Jit.RyuJit)
-//            .With(Platform.X86))
-//
-//        // Legacy / x64
-//
-//        .With(Job.Default
-//            .With(GarbageCollection (Server = true))
-//            .With(Jit.LegacyJit)
-//            .With(Platform.X64))
-//
-//        // Ryu / x64
-//
-//        .With(Job.Default
-//            .With(GarbageCollection (Server = true))
-//            .With(Jit.RyuJit)
-//            .With(Platform.X64))
-//
-//        // Diagnostics
-//
-//        // GC/Memory
-//
-//        //.With(MemoryDiagnoser ())
-//
-//        // Inlining
-//
-//        .With(InliningDiagnoser ())
-//
-//        // Exporters
-//
-//        .With(MarkdownExporter.GitHub)
+         // Jobs
 
+         // Legacy / Any
+
+        .With(Job.Default
+            .With(GarbageCollection (Server = true))
+            .With(Jit.LegacyJit)
+            .With(Platform.AnyCpu))
+
+        // Ryu / Any
+
+        .With(Job.Default
+            .With(GarbageCollection (Server = true))
+            .With(Jit.RyuJit)
+            .With(Platform.AnyCpu))
+
+        // Legacy / x86
+
+        .With(Job.Default
+            .With(GarbageCollection (Server = true))
+            .With(Jit.LegacyJit)
+            .With(Platform.X86))
+
+        // Ryu / x86
+
+        .With(Job.Default
+            .With(GarbageCollection (Server = true))
+            .With(Jit.RyuJit)
+            .With(Platform.X86))
+
+        // Legacy / x64
+
+        .With(Job.Default
+            .With(GarbageCollection (Server = true))
+            .With(Jit.LegacyJit)
+            .With(Platform.X64))
+
+        // Ryu / x64
+
+        .With(Job.Default
+            .With(GarbageCollection (Server = true))
+            .With(Jit.RyuJit)
+            .With(Platform.X64))
+
+        // Diagnostics
+
+        // GC/Memory
+
+        .With(MemoryDiagnoser ())
+
+        // Inlining
+
+        .With(InliningDiagnoser ())
+
+        // Exporters
+
+        .With(MarkdownExporter.GitHub)
 
 // Main
 
